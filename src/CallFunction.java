0b2fbd84b5bb7eb562d46a6711161cd31e320307
Feris-Alexsy <ahmadlamborghiniavetador>
2022-09-06 17:38:52 +0700

    Initial commit

---
+++ src/CallFunction.java
@@ -0,0 +1,11 @@
+public class CallFunction {
+
+    public static void main(String[] args) {
+        //memanggil fungsi
+        cobaFungsi();
+    }
+
+    public static void cobaFungsi() {
+        System.out.println("Ini merupakan bagian dari funsi");
+    }
+}
